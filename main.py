010ce9f92234b8b08b30683dac960d168f832b20
pepperoni505 <49950581+pepperoni505@users.noreply.github.com>
2021-06-08 08:43:00 -0400

    feat: day/night animation

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ dirname = os.path.dirname(__file__)
 
 fbw_tail = os.path.join(dirname, 'images/FBW-Tail.png')
 obamium = os.path.join(dirname, 'images/obamium')
+entire_cycle = os.path.join(dirname, 'images/03_entire_cycle')
 
-with open(os.path.join(obamium, 'canvas.json')) as f:
+with open(os.path.join(entire_cycle, 'canvas.json')) as f:
     canvas_json = json.load(f)
 
 async def main():
-    auto_draw = AutoDrawer(client, obamium, canvas_json['left'], canvas_json['top'], is_animated=True)
+    auto_draw = AutoDrawer(client, entire_cycle, canvas_json['left'], canvas_json['top'], is_animated=True)
     await auto_draw.draw(is_guarded=True)
 
 asyncio.run(main())
